0fef0919f5b8eeec8ad002804ed3c57420460bdb
JairoARojasR <97000216+JairoARojasR@users.noreply.github.com>
2024-06-15 17:27:48 -0500

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
 //export const base_url = "https://localhost:5000/api/";
-export const base_url = "https://aysredomaapi.netlify.app/api/";
+export const base_url = "https://autorepuestos-la-redoma.up.railway.app/api/";
 
